b3b5cccc2818c80f607b1e1cedeebc75829bf020
Haden Bennett <hadenbennett444@gmail.com>
2021-10-20 14:55:09 -0500

    Updates to Modals

--- app/screens/ClassScreen.js
+++ app/screens/ClassScreen.js
@@ -1,66 +1,65 @@
-import React, {useEffect,userContext} from 'react';
-import {StyleSheet, Text, Button, View, TouchableOpacity} from 'react-native'
-import ClassList from '../components/ClassList'
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addClass } from '../actions/actions.js';
-
+import React, { useEffect, userContext } from "react";
+import { StyleSheet, Text, Button, View, TouchableOpacity } from "react-native";
+import ClassList from "../components/ClassList";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addClass } from "../actions/actions.js";
+import { globalStyles } from "../global";
 
 //This is the Screen displaying the list of Classes the teacher has
 
-const ClassScreen = ({navigation} ) => {
-    const [clas, setClass] = useState('');
-    const dispatch = useDispatch();
-    const submitClass = (clas) => dispatch(addClass(clas));
+const ClassScreen = ({ navigation }) => {
+  const [clas, setClass] = useState("");
+  const dispatch = useDispatch();
+  const submitClass = (clas) => dispatch(addClass(clas));
 
-    return (
-        <View style={styles.container}>
-        <Text style={styles.baseText}>Class</Text>
-        <ClassList/>
-        <TouchableOpacity style={styles.addClassButton} 
+  return (
+    <View style={styles.container}>
+      <Text style={styles.baseText}>Class</Text>
+      <ClassList />
+      <TouchableOpacity
+        style={styles.addClassButton}
         onPress={() => submitClass("Class 4")}
-            ><Text>ADD CLASS</Text></TouchableOpacity>
+      >
+        <Text>ADD CLASS</Text>
+      </TouchableOpacity>
     </View>
-
-    );
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    baseText: {
-        color: "black",
-        textAlign: "left",
-        fontWeight: "bold",
-        marginTop: 50,
-        fontSize: 30,
-        textAlignVertical: "center",
-        left: 6
-    },
-    container: {
-        flex: 1,
-        backgroundColor: '#FCFCFC',
-      },
-    addClassButton: {
-        justifyContent: 'center',
-        textAlign: 'center', 
-        fontWeight: 'bold',
-        fontSize: 30,
-        position: 'absolute',
-        left: 135,
-        bottom: 20,
-        backgroundColor: 'orange',
-        width: 100,
-        height: 50,
-    }
-})
+  background: {
+    flex: 1,
+    backgroundColor: "white",
+    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  baseText: {
+    color: "black",
+    textAlign: "left",
+    fontWeight: "bold",
+    marginTop: 50,
+    fontSize: 30,
+    textAlignVertical: "center",
+    left: 6,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#FCFCFC",
+  },
+  addClassButton: {
+    justifyContent: "center",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 30,
+    position: "absolute",
+    left: 135,
+    bottom: 20,
+    backgroundColor: "orange",
+    width: 100,
+    height: 50,
+  },
+});
 
 export default ClassScreen;
-
-
-
